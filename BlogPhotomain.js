219b86d88cae7c352d5bd8a5a4ad56425187c1d3
AdarshKumarDash <86519736+AdarshKumarDash@users.noreply.github.com>
2021-06-28 21:05:12 +0530

    Add files via upload

---
+++ BlogPhotomain.js
@@ -0,0 +1,9 @@
+function show1() {
+    window.alert("The bird name is Woodpecker");
+}
+function show2() {
+    window.alert("The bird name is Budgy");
+}
+function show3() {
+    window.alert("The bird name is Sparrow");
+}
